fed967acc6dabad205b09cee43479d3565a1ca1f
Om G <ogavade.isa.ae>
2024-03-25 17:21:13 +0530

    Update 1 on Layout Designer

--- online-seats-booking-app/src/components/Pages/layout/SeatGrid.jsx
+++ online-seats-booking-app/src/components/Pages/layout/SeatGrid.jsx
@@ -1,23 +1,46 @@
+import { useState } from "react";
 import styles from "./SeatLayout.module.css";
 
 export const SeatGrid = (props) => {
+  // bY CHANGING ELEMENTS OF THIS ARRAY we can alter number of rows and columns
+  // Later from backend we will fetch an array like below and modify our code to work for the elements inside it
+  // for now we can set the lenght from props and alter the styling
+  const [items, setItems] = useState(
+    Array.from({ length: props.rows * props.columns }).fill("white")
+  );
 
-    // bY CHANGING ELEMENTS OF THIS ARRAY we can alter number of rows and columns
-    // Later from backend we will fetch an array like below and modify our code to work for the elements inside it
-    // for now we can set the lenght from props and alter the styling
-  const items = Array.from({ length: (props.rows*props.columns) }).fill(0);
+  const handleClick = (seatIndex) => {
+    // Create a copy of the seatColors array to avoid mutation
+    const updatedColors = [...items];
+
+    if (updatedColors[seatIndex] == "white") {
+      updatedColors[seatIndex] = "green";
+      props.onSelect(-1);
+
+    } else if (updatedColors[seatIndex] == "green") {
+      updatedColors[seatIndex] = "white";
+      props.onSelect(1);
+    }
+
+    // Update the state with the modified array
+    setItems(updatedColors);
+  };
+
   var gridStyles = {
-    gridTemplateColumns: 'repeat('+props.columns+', 1fr)',
-    gridTemplateRows: 'repeat('+props.rows+', 1fr)'
-  }
+    gridTemplateColumns: "repeat(" + props.columns + ", 1fr)",
+    gridTemplateRows: "repeat(" + props.rows + ", 1fr)",
+  };
   return (
     <div>
-      <div className={styles.innerGridContainer} 
-            style={gridStyles}
-      >
-        {
-        items.map((item) => <div className={styles.seat}></div>)
-        }
+      <div className={styles.innerGridContainer} style={gridStyles}>
+        {items.map((color, index) => (
+          <div
+            key={index}
+            className={styles.seat}
+            style={{ backgroundColor: color }}
+            onClick={() => handleClick(index)}
+          ></div>
+        ))}
       </div>
     </div>
   );
--- online-seats-booking-app/src/components/Pages/layout/SeatLayout.jsx
+++ online-seats-booking-app/src/components/Pages/layout/SeatLayout.jsx
@@ -1,28 +1,49 @@
+import { useState } from "react";
 import { SeatGrid } from "./SeatGrid";
 import styles from "./SeatLayout.module.css";
 
 export const SeatLayout = () => {
   const items = [1, 2, 3, 4, 5, 6];
+  var [noOfSeats, setNoOfSeats] = useState(128); 
 
+  const handleReset = () => {
+    window.location.reload();
+  }
+
+  const updateCount = (value) => {
+
+    setNoOfSeats(value + noOfSeats);
+  }
+
+  
   return (
     <div>
       <div className={styles.conatiner}>
+        <div className={styles.buttonsContainer}>
+          <div className={styles.smallContainer}>
+            <button className={styles.resetButton} onClick={()=>handleReset()}>Reset</button>
+            <button className={styles.saveButton}>Save</button>
+          </div>
+        </div>
+
         <div className={styles.outerGrid}>
           {items.map((item) => {
-
             if (item == 2 || item == 5) {
-
-              return <div className={styles.outerGridItem}>
-                     <SeatGrid rows="4" columns="4"></SeatGrid></div>
+              return (
+                <div className={styles.outerGridItem}>
+                  <SeatGrid rows="4" columns="4" onSelect ={updateCount}></SeatGrid>
+                </div>
+              );
             }
 
             return (
               <div className={styles.outerGridItem}>
-                <SeatGrid rows="4" columns="6"></SeatGrid>
+                <SeatGrid rows="4" columns="6" onSelect ={updateCount}></SeatGrid>
               </div>
             );
           })}
         </div>
+         <h3>Seats Available: {noOfSeats}</h3>
       </div>
     </div>
   );
